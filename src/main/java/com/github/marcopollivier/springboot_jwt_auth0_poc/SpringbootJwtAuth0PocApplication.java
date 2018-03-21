b5273ce023920b064a886a1981765929894a6006
Marco Paulo Ollivier <marcopollivier@gmail.com>
2018-03-21 15:11:10 -0300

    Cria estrutura basica do projeto

---
+++ src/main/java/com/github/marcopollivier/springboot_jwt_auth0_poc/SpringbootJwtAuth0PocApplication.java
@@ -0,0 +1,12 @@
+package com.github.marcopollivier.springboot_jwt_auth0_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJwtAuth0PocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJwtAuth0PocApplication.class, args);
+	}
+}
